485671386c492960199e22eeb7ae38e926e576bb
Tom Mahieu <hello@mahieutom.com>
2020-03-31 16:20:31 +0200

    Bootstrap the bot

--- bot.py
+++ bot.py
@@ -45,5 +45,18 @@ def send_welcome(message):
 def send_welcome(message):
     bot.reply_to(message, 'We are still developing this bot 😉')
 
+################################################################
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://technockickenbot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
